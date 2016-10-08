161b3bc86d022cd188b873b72f53a2d1e57d6e21
Marcode777 <marcosebello7@gmail.com>
2016-10-08 15:13:38 -0400

    modified src/js/pages/Todos.js, changed its h1 heading to tasks

--- src/js/pages/Todos.js
+++ src/js/pages/Todos.js
@@ -50,7 +50,7 @@ export default class Featured extends React.Component {
         <button onClick={this.createTodo.bind(this)}>CREATE TASK</button>
         <input ref="input"/>
         <button onClick={this.reloadTodos.bind(this)}>RELOAD</button>
-        <h1>Todos</h1>
+        <h1>TASKS</h1>
         <ul>{TodoComponents}</ul>
       </div>
       );
